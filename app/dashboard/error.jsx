95b608526f4da6a07b73e304e0c5679a8a732ada
Safayat <safayatofficial1@gmail.com>
2024-05-12 00:04:03 +0600

    not found & error page added

---
+++ app/dashboard/error.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const error = () => {
+    return (
+        <div className='w-screen h-screen bg-white flex items-center justify-center'>
+            <p className='text-black text-4xl font-bold'>Something went wrong</p>
+        </div>
+    )
+}
+
+export default error
